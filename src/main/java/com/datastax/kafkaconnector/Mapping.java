ec3c9397bab4f40eb72f1214aa8c45e8bc65f8e8
Sandeep Tamhankar <sandeep.tamhankar@datastax.com>
2018-08-21 22:07:20 -0700

    More alpha1 release tweaks

--- src/main/java/com/datastax/kafkaconnector/Mapping.java
+++ src/main/java/com/datastax/kafkaconnector/Mapping.java
@@ -22,7 +22,7 @@ import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * Modeled after the DefaultMapping class in DSBulk. A few key diffs: 1. "variable" in dsbulk =>
+ * Modeled after the DefaultMapping class in DSBulk. A few key diffs: 1. "variable" in dsbulk =&gt;
  * "column" here 2. the mapping in dsbulk is 1:1, whereas here we support many (columns) to 1 field.
  */
 public class Mapping {
