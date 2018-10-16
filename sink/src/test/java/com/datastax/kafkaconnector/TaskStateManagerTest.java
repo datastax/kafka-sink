b76eb8c466ac44aa83ece381fc6aa2c09da7a91e
Tomasz Lelek <tomekl007@gmail.com>
2018-10-16 17:26:35 +0200

    Kaf 55 (#47)
    * KAF-55 Extract state management to separate component TaskStateManager.
    Write test that fails on the bug that was found -> make a fix in transition between WAIT and STOP state
    
    * KAF-55 Pass LifecycleManager.stop() as a callback from caller. Generated licence files
    
    * KAF-55 add information about KAF-55 to changelog. Add description of 4th possible case to TaskManagerState transitionToStop() method
    
    * KAF-55 simplyfing code to by null-checks. Use final for fields
    
    * KAF-55 formatting
    
    * KAF-55 change test to be without Thread.sleep. Test right now is able to find deadlock if there will be no state.compareAndSet(WAIT,STOP) call inside stop method
    
    * KAF-55 test for transition to stop
    
    * KAF-55 rename properly
    
    * KAF-55 Test for stopping process that was in the RUN state
    
    * KAF-55 test for invoking stop callback method
    
    * KAF-55 optimize imports
    
    * KAF-55 reformat using mvn
    
    * KAF-56 fix deadlocking test
    
    * KAF-56 remove exceptions
    
    * KAF-56  mvn fmt:format

---
+++ sink/src/test/java/com/datastax/kafkaconnector/TaskStateManagerTest.java
@@ -0,0 +1,141 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ *   This software is subject to the below license agreement.
+ *   DataStax may make changes to the agreement from time to time,
+ *   and will post the amended terms at
+ *   https://www.datastax.com/terms/datastax-dse-bulk-utility-license-terms.
+ */
+package com.datastax.kafkaconnector;
+
+import static com.datastax.kafkaconnector.TaskStateManager.TaskState;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import org.awaitility.Duration;
+import org.junit.jupiter.api.Test;
+
+class TaskStateManagerTest {
+
+  private static final Runnable NO_OP_RUNNABLE = () -> {};
+
+  @Test
+  void shouldStartTaskAndEndInWaitState() {
+    // given
+    TaskStateManager taskStateManager = new TaskStateManager();
+
+    // when
+    taskStateManager.waitRunTransitionLogic(NO_OP_RUNNABLE);
+
+    // then
+    assertThat(taskStateManager.state.get()).isEqualTo(TaskState.WAIT);
+  }
+
+  @Test
+  void shouldStopProperlyEvenIfTheRunLogicFinishedInTheMeantime() {
+    // given
+    ExecutorService executorService = Executors.newFixedThreadPool(3);
+    CountDownLatch stopLatch = new CountDownLatch(1);
+    CountDownLatch runLatch = new CountDownLatch(1);
+    TaskStateManager taskStateManager = new TaskStateManager();
+
+    // when
+    executorService.submit(
+        () ->
+            taskStateManager.waitRunTransitionLogic(
+                () -> {
+                  try {
+                    stopLatch.await();
+                  } catch (InterruptedException e) {
+                    e.printStackTrace();
+                  }
+                }));
+
+    executorService.submit(
+        () ->
+            taskStateManager.toStopTransitionLogic(
+                () -> {
+                  try {
+                    stopLatch.countDown();
+                    runLatch.await();
+                  } catch (InterruptedException e) {
+                    e.printStackTrace();
+                  }
+                },
+                NO_OP_RUNNABLE));
+    runLatch.countDown();
+    executorService.submit(() -> taskStateManager.waitRunTransitionLogic(NO_OP_RUNNABLE));
+
+    // then
+    executorService.shutdownNow();
+    await()
+        .atMost(Duration.FIVE_SECONDS)
+        .until(() -> taskStateManager.state.get() == TaskState.STOP);
+  }
+
+  @Test
+  void shouldTransitionToStopIfRunOperationSucceeded() {
+    // given
+    TaskStateManager taskStateManager = new TaskStateManager();
+
+    // when
+    taskStateManager.waitRunTransitionLogic(NO_OP_RUNNABLE);
+    taskStateManager.toStopTransitionLogic(NO_OP_RUNNABLE, NO_OP_RUNNABLE);
+
+    // then
+    assertThat(taskStateManager.state.get()).isEqualTo(TaskState.STOP);
+  }
+
+  @Test
+  void shouldStopProperlyEvenIfTheStopOccurredDuringRunState()
+      throws ExecutionException, InterruptedException {
+    // given
+    ExecutorService executorService = Executors.newFixedThreadPool(3);
+    CountDownLatch stopLatch = new CountDownLatch(1);
+    TaskStateManager taskStateManager = new TaskStateManager();
+
+    // when
+    Future<?> runFuture =
+        executorService.submit(
+            () ->
+                taskStateManager.waitRunTransitionLogic(
+                    () -> {
+                      try {
+                        stopLatch.await();
+                      } catch (InterruptedException e) {
+                        e.printStackTrace();
+                      }
+                    }));
+    Future<?> stopFuture =
+        executorService.submit(
+            () -> taskStateManager.toStopTransitionLogic(stopLatch::countDown, NO_OP_RUNNABLE));
+    runFuture.get();
+    stopFuture.get();
+
+    // then
+    executorService.shutdown();
+    assertThat(taskStateManager.state.get()).isEqualTo(TaskState.STOP);
+  }
+
+  @Test
+  void shouldInvokeStopCallbackWhenStopTransitionLogicMethodFinished() {
+    // given
+    Runnable stopCallbackMock = mock(Runnable.class);
+    TaskStateManager taskStateManager = new TaskStateManager();
+
+    // when
+    taskStateManager.waitRunTransitionLogic(NO_OP_RUNNABLE);
+    taskStateManager.toStopTransitionLogic(NO_OP_RUNNABLE, stopCallbackMock);
+
+    // then
+    verify(stopCallbackMock, times(1)).run();
+  }
+}
