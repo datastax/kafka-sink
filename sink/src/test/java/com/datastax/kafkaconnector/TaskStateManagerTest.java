fa53c1f4a5d2e7df9f3f5e8834a34776cb050755
tomekl007 <tomasz.lelek@datastax.com>
2018-10-16 11:06:52 +0200

    KAF-55 Test for stopping process that was in the RUN state

--- sink/src/test/java/com/datastax/kafkaconnector/TaskStateManagerTest.java
+++ sink/src/test/java/com/datastax/kafkaconnector/TaskStateManagerTest.java
@@ -16,11 +16,13 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+
 import org.junit.jupiter.api.Test;
 
 class TaskStateManagerTest {
 
-  private static final Runnable NO_OP_RUNNABLE = () -> {};
+  private static final Runnable NO_OP_RUNNABLE = () -> {
+  };
 
   @Test
   void shouldStartTaskAndEndInWaitState() {
@@ -28,7 +30,8 @@ class TaskStateManagerTest {
     TaskStateManager taskStateManager = new TaskStateManager();
 
     // when
-    taskStateManager.waitRunTransitionLogic(() -> {});
+    taskStateManager.waitRunTransitionLogic(() -> {
+    });
 
     // then
     assertThat(taskStateManager.state.get()).isEqualTo(TaskState.WAIT);
@@ -88,6 +91,41 @@ class TaskStateManagerTest {
     taskStateManager.toStopTransitionLogic(NO_OP_RUNNABLE, NO_OP_RUNNABLE);
 
     // then
+    assertThat(taskStateManager.state.get()).isEqualTo(TaskState.STOP);
+  }
+
+  @Test
+  void shouldStopProperlyEvenIfTheStopOccurredDuringRunState()
+      throws ExecutionException, InterruptedException {
+    // given
+    ExecutorService executorService = Executors.newFixedThreadPool(3);
+    CountDownLatch stopLatch = new CountDownLatch(1);
+    TaskStateManager taskStateManager = new TaskStateManager();
+
+    // when
+    Future<?> runFuture =
+        executorService.submit(
+            () ->
+                taskStateManager.waitRunTransitionLogic(
+                    () -> {
+                      try {
+                        stopLatch.await();
+                      } catch (InterruptedException e) {
+                        e.printStackTrace();
+                      }
+                    }));
+    Future<?> stopFuture =
+        executorService.submit(
+            () ->
+                taskStateManager.toStopTransitionLogic(
+                    stopLatch::countDown,
+                    NO_OP_RUNNABLE)
+        );
+    runFuture.get();
+    stopFuture.get();
+
+    // then
+    executorService.shutdown();
     assertThat(taskStateManager.state.get()).isEqualTo(TaskState.STOP);
   }
 }
