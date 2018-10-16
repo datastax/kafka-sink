1d404ca40f99bdbb25c414b0413f3ba92276bf06
tomekl007 <tomasz.lelek@datastax.com>
2018-10-16 10:49:39 +0200

    KAF-55 change test to be without Thread.sleep. Test right now is able to find deadlock if there will be no state.compareAndSet(WAIT,STOP) call inside stop method

--- sink/src/test/java/com/datastax/kafkaconnector/TaskStateManagerTest.java
+++ sink/src/test/java/com/datastax/kafkaconnector/TaskStateManagerTest.java
@@ -13,6 +13,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
 import org.awaitility.Duration;
 import org.junit.jupiter.api.Test;
 
@@ -24,51 +30,53 @@ class TaskStateManagerTest {
     TaskStateManager taskStateManager = new TaskStateManager();
 
     // when
-    taskStateManager.waitRunTransitionLogic(() -> {});
+    taskStateManager.waitRunTransitionLogic(() -> {
+    });
 
     // then
     assertThat(taskStateManager.state.get()).isEqualTo(TaskState.WAIT);
   }
 
   @Test
-  void shouldStopProperlyEvenIfTheRunLogicFinishedInTheMeantime() {
+  void shouldStopProperlyEvenIfTheRunLogicFinishedInTheMeantime() throws ExecutionException, InterruptedException {
     // given
-    CountDownLatch countDownLatch = new CountDownLatch(1);
+    ExecutorService executorService = Executors.newFixedThreadPool(3);
+    CountDownLatch stopLatch = new CountDownLatch(1);
+    CountDownLatch putLatch = new CountDownLatch(1);
     TaskStateManager taskStateManager = new TaskStateManager();
 
     // when
-    Thread putOperation =
-        new Thread(
-            () ->
-                taskStateManager.waitRunTransitionLogic(
-                    () -> {
-                      try {
-                        countDownLatch.await();
-                        Thread.sleep(100);
-                      } catch (InterruptedException e) {
-                        e.printStackTrace();
-                      }
-                    }));
-    Thread stopOperation =
-        new Thread(
-            () ->
-                taskStateManager.toStopTransitionLogic(
-                    () -> {
-                      try {
-                        countDownLatch.await();
-                        Thread.sleep(1000);
-                      } catch (InterruptedException e) {
-                        e.printStackTrace();
-                      }
-                    },
-                    () -> {}));
-    putOperation.start();
-    stopOperation.start();
-    countDownLatch.countDown();
+    Future<?> putFuture = executorService.submit(
+        () ->
+            taskStateManager.waitRunTransitionLogic(
+                () -> {
+                  try {
+                    stopLatch.await();
+                  } catch (InterruptedException e) {
+                    e.printStackTrace();
+                  }
+                }));
+    Future<?> stopFuture = executorService.submit(
+        () ->
+            taskStateManager.toStopTransitionLogic(
+                () -> {
+                  try {
+                    stopLatch.countDown();
+                    putLatch.await();
+                  } catch (InterruptedException e) {
+                    e.printStackTrace();
+                  }
+                },
+                () -> {
+                }));
+    putFuture.get();
+    putLatch.countDown();
+    executorService.submit(() -> taskStateManager.waitRunTransitionLogic(() -> {
+    })).get();
+    stopFuture.get();
 
     // then
-    await()
-        .atMost(Duration.FIVE_SECONDS)
-        .until(() -> taskStateManager.state.get() == TaskState.STOP);
+    executorService.shutdown();
+    assertThat(taskStateManager.state.get()).isEqualTo(TaskState.STOP);
   }
 }
