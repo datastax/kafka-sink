fcb1c871931ca7e6f11d90e8f12ecf26f3d2f6e2
tomekl007 <tomasz.lelek@datastax.com>
2018-10-18 10:49:25 +0200

    KAF-56 test for schema and for case when list is send as a Value

--- sink/src/test/java/com/datastax/kafkaconnector/metadata/MetadataCreatorTest.java
+++ sink/src/test/java/com/datastax/kafkaconnector/metadata/MetadataCreatorTest.java
@@ -10,17 +10,28 @@ package com.datastax.kafkaconnector.metadata;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
+import com.datastax.oss.driver.api.core.type.reflect.GenericType;
+import com.datastax.oss.driver.internal.core.type.PrimitiveType;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
+
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+
+
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
 import org.junit.jupiter.api.Test;
 
 class MetadataCreatorTest {
+
+  private static final PrimitiveType CQL_TYPE = new PrimitiveType(-1);
+  private static final GenericType<JsonNode> JSON_NODE_GENERIC_TYPE = GenericType.of(JsonNode.class);
 
   @Test
   void shouldCreateMetadataForStruct() throws IOException {
@@ -40,6 +51,8 @@ class MetadataCreatorTest {
     assertThat(innerDataAndMetadata.getInnerData().getFieldValue("name")).isEqualTo("Bobby McGee");
     assertThat(innerDataAndMetadata.getInnerData().getFieldValue("age")).isEqualTo(21);
     assertThat(innerDataAndMetadata.getInnerMetadata()).isNotNull();
+    assertThat(innerDataAndMetadata.getInnerMetadata().getFieldType("name", CQL_TYPE)).isEqualTo(GenericType.STRING);
+    assertThat(innerDataAndMetadata.getInnerMetadata().getFieldType("age", CQL_TYPE)).isEqualTo(GenericType.INTEGER);
   }
 
   @Test
@@ -54,6 +67,7 @@ class MetadataCreatorTest {
     assertThat(((TextNode) innerDataAndMetadata.getInnerData().getFieldValue("name")).textValue())
         .isEqualTo("Mike");
     assertThat(innerDataAndMetadata.getInnerMetadata()).isNotNull();
+    assertThat(innerDataAndMetadata.getInnerMetadata().getFieldType("name", CQL_TYPE)).isEqualTo(GenericType.of(JsonNode.class));
   }
 
   @Test
@@ -66,11 +80,12 @@ class MetadataCreatorTest {
 
     // then
     assertThat(
-            ((ObjectNode) innerDataAndMetadata.getInnerData().getFieldValue("name"))
-                .get("name2")
-                .textValue())
+        ((ObjectNode) innerDataAndMetadata.getInnerData().getFieldValue("name"))
+            .get("name2")
+            .textValue())
         .isEqualTo("Mike");
     assertThat(innerDataAndMetadata.getInnerMetadata()).isNotNull();
+    assertThat(innerDataAndMetadata.getInnerMetadata().getFieldType("name", CQL_TYPE)).isEqualTo(JSON_NODE_GENERIC_TYPE);
   }
 
   @Test
@@ -84,6 +99,7 @@ class MetadataCreatorTest {
     // then
     assertThat(innerDataAndMetadata.getInnerData().getFieldValue("name")).isEqualTo(incorrectJson);
     assertThat(innerDataAndMetadata.getInnerMetadata()).isNotNull();
+    assertThat(innerDataAndMetadata.getInnerMetadata().getFieldType("name", CQL_TYPE)).isEqualTo(GenericType.STRING);
   }
 
   @Test
@@ -99,5 +115,24 @@ class MetadataCreatorTest {
     assertThat(((TextNode) innerDataAndMetadata.getInnerData().getFieldValue("f_1")).textValue())
         .isEqualTo("v_1");
     assertThat(innerDataAndMetadata.getInnerMetadata()).isNotNull();
+    assertThat(innerDataAndMetadata.getInnerMetadata().getFieldType("f_1", CQL_TYPE)).isEqualTo(JSON_NODE_GENERIC_TYPE);
+  }
+
+  @Test
+  void shouldCreateMetadataFromMapWithListField() throws IOException {
+    // given
+    Map<String, Object> fields = new HashMap<>();
+    fields.put("f_1", Arrays.asList("1", "2", "3"));
+
+    // when
+    InnerDataAndMetadata innerDataAndMetadata = MetadataCreator.makeMeta(fields);
+
+    // then
+    ArrayNode f_1Value = (ArrayNode) innerDataAndMetadata.getInnerData().getFieldValue("f_1");
+    assertThat(f_1Value.get(0).textValue()).isEqualTo("1");
+    assertThat(f_1Value.get(1).textValue()).isEqualTo("2");
+    assertThat(f_1Value.get(2).textValue()).isEqualTo("3");
+    assertThat(innerDataAndMetadata.getInnerMetadata()).isNotNull();
+    assertThat(innerDataAndMetadata.getInnerMetadata().getFieldType("f_1", CQL_TYPE)).isEqualTo(JSON_NODE_GENERIC_TYPE);
   }
 }
