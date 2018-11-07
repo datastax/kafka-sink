2f508f40d6805964405f4a2d60c8db2b301159cc
tomekl007 <tomasz.lelek@datastax.com>
2018-11-07 13:44:00 +0100

    KAF-76 apply PR remarks, add documentation

--- sink/src/main/java/com/datastax/kafkaconnector/config/ContactPointsValidator.java
+++ sink/src/main/java/com/datastax/kafkaconnector/config/ContactPointsValidator.java
@@ -11,24 +11,33 @@ package com.datastax.kafkaconnector.config;
 import com.datastax.oss.driver.shaded.guava.common.net.InetAddresses;
 import com.google.common.net.InternetDomainName;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.kafka.common.config.ConfigException;
-import org.jetbrains.annotations.NotNull;
 
 public class ContactPointsValidator {
 
+  /**
+   * * This method validates all contact points and throws ConfigException if there is at least one
+   * invalid. The correct contact point is proper IPv4 address in a format X.X.X.X or correct domain
+   * name according to RFC: http://www.ietf.org/rfc/rfc3490.txt
+   *
+   * @param contactPoints
+   */
   public static void validateContactPoints(List<String> contactPoints) {
-    String errorMsg =
-        contactPoints.stream().filter(isCorrectIpAddressOrDns()).collect(Collectors.joining(","));
-    if (!errorMsg.isEmpty()) {
+    Set<String> invalid =
+        contactPoints
+            .stream()
+            .filter(ContactPointsValidator::isInvalidAddress)
+            .collect(Collectors.toSet());
+    if (!invalid.isEmpty()) {
       throw new ConfigException(
-          String.format("Incorrect %s: %s", DseSinkConfig.CONTACT_POINTS_OPT, errorMsg));
+          String.format(
+              "Incorrect %s: %s", DseSinkConfig.CONTACT_POINTS_OPT, String.join(",", invalid)));
     }
   }
 
-  @NotNull
-  private static Predicate<String> isCorrectIpAddressOrDns() {
-    return cp -> !(InetAddresses.isInetAddress(cp) || InternetDomainName.isValid(cp));
+  private static boolean isInvalidAddress(String contactPoint) {
+    return !InetAddresses.isInetAddress(contactPoint) && !InternetDomainName.isValid(contactPoint);
   }
 }
