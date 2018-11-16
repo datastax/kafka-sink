f28e1caea6169e70f2bb3d0af864b8f6af718923
tomekl007 <tomasz.lelek@datastax.com>
2018-11-16 16:07:18 +0100

    KAF-85 don't need to sanitize, use asInternal instead

--- sink/src/main/java/com/datastax/kafkaconnector/metrics/MetricNamesCreator.java
+++ sink/src/main/java/com/datastax/kafkaconnector/metrics/MetricNamesCreator.java
@@ -14,13 +14,7 @@ public class MetricNamesCreator {
 
   public static String createBatchSizeMetricName(
       String taskName, CqlIdentifier keyspace, CqlIdentifier table) {
-    return String.format("%s/%s/%s/batchSize", taskName, sanitize(keyspace), sanitize(table));
-  }
-
-  private static String sanitize(CqlIdentifier identifier) {
-    String sanitized = identifier.asInternal();
-    // remove any slashes from CQL identifier names as they will interfere with hierarchical names
-    return sanitized.replace("//", "");
+    return String.format("%s/%s/%s/batchSize", taskName, keyspace.asInternal(), table.asInternal());
   }
 
   public static String createDriverMetricName(String name) {
