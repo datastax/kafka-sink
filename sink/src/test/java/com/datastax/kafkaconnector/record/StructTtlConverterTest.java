84b6c2e8647080fc1ab79d769b41d729df8901b8
tomekl007 <tomasz.lelek@datastax.com>
2019-04-08 15:48:40 +0200

    KAF-107 fix tests

--- sink/src/test/java/com/datastax/kafkaconnector/record/StructTtlConverterTest.java
+++ sink/src/test/java/com/datastax/kafkaconnector/record/StructTtlConverterTest.java
@@ -30,7 +30,7 @@ class StructTtlConverterTest {
 
   private static Stream<? extends Arguments> expectedToSeconds() {
     return Stream.of(
-        Arguments.of(1000D, 1.0F),
+        Arguments.of(1000F, 1.0F),
         Arguments.of(1000D, 1D),
         Arguments.of(1000L, 1L),
         Arguments.of(1000, 1));
