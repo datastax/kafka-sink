a7f9898532af750aa6a6a4c36c91680a3b52720c
tomekl007 <tomasz.lelek@datastax.com>
2019-05-08 16:09:49 +0200

    KAF-109 not need to copy certifyConfluent.sh script to remote

--- certify_confluent/certifyConfluent.sh
+++ certify_confluent/certifyConfluent.sh
@@ -26,7 +26,6 @@ echo "smoke test confluent 5.2"
 ctool --provider=ironic launch -p devtools-ironic kct 1
 ctool run kct "mkdir /tmp/dse-connector"
 ctool scp -R kct 0 ${CONNECTOR_JAR_LOCATION} /tmp/dse-connector
-ctool scp -R kct 0 ${KAFKA_SINK_REPO_LOCATION}/certify_confluent/certifyConfluent.sh /home/automaton
 ctool scp -R kct 0 ${KAFKA_SINK_REPO_LOCATION}/certify_confluent/certifyConfluentVersion.sh /home/automaton
 ctool run --sudo kct "chmod 777 /home/automaton/certifyConfluent.sh; chmod 777 /home/automaton/certifyConfluentVersion.sh"
 ctool run kct "~/certifyConfluentVersion.sh "5.2" "${DSE_CONNECTOR_VERSION}" &> certify_confluent_5.2.log"
@@ -38,7 +37,6 @@ echo "smoke test confluent 5.1"
 ctool --provider=ironic launch -p devtools-ironic kct 1
 ctool run kct "mkdir /tmp/dse-connector"
 ctool scp -R kct 0 ${CONNECTOR_JAR_LOCATION} /tmp/dse-connector
-ctool scp -R kct 0 ${KAFKA_SINK_REPO_LOCATION}/certify_confluent/certifyConfluent.sh /home/automaton
 ctool scp -R kct 0 ${KAFKA_SINK_REPO_LOCATION}/certify_confluent/certifyConfluentVersion.sh /home/automaton
 ctool run --sudo kct "chmod 777 /home/automaton/certifyConfluent.sh; chmod 777 /home/automaton/certifyConfluentVersion.sh"
 ctool run kct "~/certifyConfluentVersion.sh "5.1" "${DSE_CONNECTOR_VERSION}" &> certify_confluent_5.1.log"
@@ -50,7 +48,6 @@ echo "smoke test confluent 5.0"
 ctool --provider=ironic launch -p devtools-ironic kct 1
 ctool run kct "mkdir /tmp/dse-connector"
 ctool scp -R kct 0 ${CONNECTOR_JAR_LOCATION} /tmp/dse-connector
-ctool scp -R kct 0 ${KAFKA_SINK_REPO_LOCATION}/certify_confluent/certifyConfluent.sh /home/automaton
 ctool scp -R kct 0 ${KAFKA_SINK_REPO_LOCATION}/certify_confluent/certifyConfluentVersion.sh /home/automaton
 ctool run --sudo kct "chmod 777 /home/automaton/certifyConfluent.sh; chmod 777 /home/automaton/certifyConfluentVersion.sh"
 ctool run kct "~/certifyConfluentVersion.sh "5.0" "${DSE_CONNECTOR_VERSION}" &> certify_confluent_5.0.log"
@@ -62,7 +59,6 @@ echo "smoke test confluent 4.1"
 ctool --provider=ironic launch -p devtools-ironic kct 1
 ctool run kct "mkdir /tmp/dse-connector"
 ctool scp -R kct 0 ${CONNECTOR_JAR_LOCATION} /tmp/dse-connector
-ctool scp -R kct 0 ${KAFKA_SINK_REPO_LOCATION}/certify_confluent/certifyConfluent.sh /home/automaton
 ctool scp -R kct 0 ${KAFKA_SINK_REPO_LOCATION}/certify_confluent/certifyConfluentVersion.sh /home/automaton
 ctool run --sudo kct "chmod 777 /home/automaton/certifyConfluent.sh; chmod 777 /home/automaton/certifyConfluentVersion.sh"
 ctool run kct "~/certifyConfluentVersion.sh "4.1" "${DSE_CONNECTOR_VERSION}" &> certify_confluent_4.1.log"
@@ -73,7 +69,6 @@ echo "smoke test confluent 4.0"
 ctool --provider=ironic launch -p devtools-ironic kct 1
 ctool run kct "mkdir /tmp/dse-connector"
 ctool scp -R kct 0 ${CONNECTOR_JAR_LOCATION} /tmp/dse-connector
-ctool scp -R kct 0 ${KAFKA_SINK_REPO_LOCATION}/certify_confluent/certifyConfluent.sh /home/automaton
 ctool scp -R kct 0 ${KAFKA_SINK_REPO_LOCATION}/certify_confluent/certifyConfluentVersion.sh /home/automaton
 ctool run --sudo kct "chmod 777 /home/automaton/certifyConfluent.sh; chmod 777 /home/automaton/certifyConfluentVersion.sh"
 ctool run kct "~/certifyConfluentVersion.sh "4.0" "${DSE_CONNECTOR_VERSION}" &> certify_confluent_4.0.log"
@@ -84,7 +79,6 @@ echo "smoke test confluent 3.3"
 ctool --provider=ironic launch -p devtools-ironic kct 1
 ctool run kct "mkdir /tmp/dse-connector"
 ctool scp -R kct 0 ${CONNECTOR_JAR_LOCATION} /tmp/dse-connector
-ctool scp -R kct 0 ${KAFKA_SINK_REPO_LOCATION}/certify_confluent/certifyConfluent.sh /home/automaton
 ctool scp -R kct 0 ${KAFKA_SINK_REPO_LOCATION}/certify_confluent/certifyConfluentVersion.sh /home/automaton
 ctool run --sudo kct "chmod 777 /home/automaton/certifyConfluent.sh; chmod 777 /home/automaton/certifyConfluentVersion.sh"
 ctool run kct "~/certifyConfluentVersion.sh "3.3" "${DSE_CONNECTOR_VERSION}" &> certify_confluent_3.3.log"
