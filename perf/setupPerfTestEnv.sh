e593c322b679fdf1625ae2106b6a45639b83e102
tomekl007 <tomasz.lelek@datastax.com>
2019-05-13 14:57:46 +0200

    KAF-116 add avro perf-test runner

--- perf/setupPerfTestEnv.sh
+++ perf/setupPerfTestEnv.sh
@@ -125,6 +125,12 @@ ctool run kc-brokers 0 "cd kafka-examples/producers; mvn clean compile exec:java
 
 start_json_test
 
+# Produce 200_000_000 avro record
+ctool run kc-brokers 0 "cd kafka-examples/producers; mvn clean compile exec:java -Dexec.mainClass=avro.AvroProducer -Dexec.args=avro-stream 200000000"
+
+start_avro_test
+
+
 
 start_json_test(){
     DSE_FIRST_ADDRESS=`ctool info --public-ips kc-dse -n 0`
@@ -138,11 +144,27 @@ start_json_test(){
     # ctool run kc-dse 0 "dse/bin/dsbulk count -k stocks -t ticks"
 }
 
+start_avro_test(){
+    DSE_FIRST_ADDRESS=`ctool info --public-ips kc-dse -n 0`
+    DSE_SECOND_ADDRESS=`ctool info --public-ips kc-dse -n 1`
+    # Submit connector task
+    sed -i "s/{dse_contact_point_1}/$DSE_FIRST_ADDRESS/g" dse-sink-avro.json
+    sed -i "s/{dse_contact_point_2}/$DSE_SECOND_ADDRESS/g" dse-sink-avro.json
+    curl -X POST -H "Content-Type: application/json" -d @dse-sink-avro.json "$CONNECT_FIRST_ADDRESS:8083/connectors"
+
+    # WAIT FOR COMPLETE && validate number of inserted records using DSBULK:
+    # ctool run kc-dse 0 "dse/bin/dsbulk count -k kafka_examples -t avro_udt_table"
+}
+
 truncate_json_dse_table(){
     ctool run kc-dse "cqlsh -e \"TRUNCATE stocks.ticks;\""
     ctool run kc-dse 'nodetool clearsnapshot --all'
 }
 
+truncate_avro_dse_table(){
+    ctool run kc-dse "cqlsh -e \"TRUNCATE kafka_examples.avro_udt_table;\""
+    ctool run kc-dse 'nodetool clearsnapshot --all'
+}
 stop_kafka_broker_services(){
     # kill kafka connect-worker
     ctool run kc-connect all "kill -9 `lsof -t -i:8083`"
