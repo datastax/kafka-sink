98347c3b7caccbee74ac38f18b91bc7f8307818b
tomekl007 <tomasz.lelek@datastax.com>
2019-10-09 12:57:47 +0200

    KAF-143 generate licence headers

--- sink/src/it/java/com/datastax/kafkaconnector/ccm/CloudSniEndToEndCCMIT.java
+++ sink/src/it/java/com/datastax/kafkaconnector/ccm/CloudSniEndToEndCCMIT.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ *   This software is subject to the below license agreement.
+ *   DataStax may make changes to the agreement from time to time,
+ *   and will post the amended terms at
+ *   https://www.datastax.com/terms/datastax-apache-kafka-connector-license-terms.
+ */
 package com.datastax.kafkaconnector.ccm;
 
 import static com.datastax.kafkaconnector.config.DseSinkConfig.SECURE_CONNECT_BUNDLE_OPT;
