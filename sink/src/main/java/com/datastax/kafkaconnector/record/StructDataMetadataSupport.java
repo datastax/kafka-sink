67a9baaff366dff2e7bab46ca94621ea6a091483
tomekl007 <tomasz.lelek@datastax.com>
2019-10-17 11:03:43 +0200

    KAF-143 add header

--- sink/src/main/java/com/datastax/kafkaconnector/record/StructDataMetadataSupport.java
+++ sink/src/main/java/com/datastax/kafkaconnector/record/StructDataMetadataSupport.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ *   This software is subject to the below license agreement.
+ *   DataStax may make changes to the agreement from time to time,
+ *   and will post the amended terms at
+ *   https://www.datastax.com/terms/datastax-apache-kafka-connector-license-terms.
+ */
 package com.datastax.kafkaconnector.record;
 
 import com.datastax.oss.driver.api.core.type.reflect.GenericType;
