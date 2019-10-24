fbea53c36b60752dfd5cebf03d98893a84f2a609
tomekl007 <tomasz.lelek@datastax.com>
2019-10-24 21:45:43 +0200

    KAF-80 validate that EndPoint is unresolved

--- sink/src/it/java/com/datastax/kafkaconnector/ccm/LifeCycleManagerIT.java
+++ sink/src/it/java/com/datastax/kafkaconnector/ccm/LifeCycleManagerIT.java
@@ -9,6 +9,7 @@
 package com.datastax.kafkaconnector.ccm;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import com.datastax.dsbulk.commons.tests.ccm.CCMCluster;
 import com.datastax.dsbulk.commons.tests.ccm.CCMExtension;
@@ -20,6 +21,7 @@ import com.datastax.oss.driver.api.core.metadata.EndPoint;
 import com.google.common.collect.ImmutableMap;
 import java.net.InetSocketAddress;
 import java.util.Map;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -52,9 +54,10 @@ public class LifeCycleManagerIT {
       set = session.execute("select * from system.local");
       assertThat(set).isNotNull();
       // and endPoint uses unresolved DNS address
-      EndPoint endPoint = session.getMetadata().getNodes().values().iterator().next().getEndPoint();
+      EndPoint endPoint = getEndPoint(session);
       assertThat(endPoint.toString())
           .isEqualTo(String.format("%s:%d", contactPointDns, ccm.getBinaryPort()));
+      assertTrue(((InetSocketAddress) getEndPoint(session).resolve()).isUnresolved());
     }
   }
 
@@ -77,9 +80,15 @@ public class LifeCycleManagerIT {
       set = session.execute("select * from system.local");
       assertThat(set).isNotNull();
       // and endPoint uses IP address
-      EndPoint endPoint = session.getMetadata().getNodes().values().iterator().next().getEndPoint();
+      EndPoint endPoint = getEndPoint(session);
       assertThat(endPoint.toString())
           .isEqualTo(String.format("%s:%d", contactPointIp, ccm.getBinaryPort()));
+      assertTrue(((InetSocketAddress) getEndPoint(session).resolve()).isUnresolved());
     }
+  }
+
+  @NotNull
+  private EndPoint getEndPoint(CqlSession session) {
+    return session.getMetadata().getNodes().values().iterator().next().getEndPoint();
   }
 }
