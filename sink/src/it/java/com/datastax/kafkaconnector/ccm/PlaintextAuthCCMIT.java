52749af369c4c5042425ef0b56a5256b62f9eeab
Tomasz Lelek <tomasz.lelek@datastax.com>
2020-01-13 08:45:27 +0100

    KAF-163: Improve UX of auth configuration by inferring auth.provider to DSE when both username and password are provided. (#99)

--- sink/src/it/java/com/datastax/kafkaconnector/ccm/PlaintextAuthCCMIT.java
+++ sink/src/it/java/com/datastax/kafkaconnector/ccm/PlaintextAuthCCMIT.java
@@ -17,15 +17,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import com.datastax.dsbulk.commons.tests.ccm.CCMCluster;
 import com.datastax.dsbulk.commons.tests.ccm.annotations.CCMConfig;
 import com.datastax.dsbulk.commons.tests.driver.annotations.SessionConfig;
+import com.datastax.kafkaconnector.config.AuthenticatorConfig.Provider;
 import com.datastax.oss.driver.api.core.AllNodesFailedException;
 import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.Row;
 import com.datastax.oss.driver.shaded.guava.common.collect.ImmutableMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @SuppressWarnings("ConstantConditions")
 @CCMConfig(
@@ -39,14 +43,9 @@ class PlaintextAuthCCMIT extends EndToEndCCMITBase {
     super(ccm, session);
   }
 
-  @Test
-  void raw_bigint_value() {
-    Map<String, String> extras =
-        ImmutableMap.<String, String>builder()
-            .put(USERNAME_OPT, "cassandra")
-            .put(PASSWORD_OPT, "cassandra")
-            .build();
-
+  @ParameterizedTest(name = "[{index}] extras={0}")
+  @MethodSource("correctCredentialsProvider")
+  void should_insert_successfully_with_correct_credentials(Map<String, String> extras) {
     conn.start(makeConnectorProperties(extras));
 
     SinkRecord record = new SinkRecord("mytopic", 0, null, null, null, 5725368L, 1234L);
@@ -57,22 +56,58 @@ class PlaintextAuthCCMIT extends EndToEndCCMITBase {
     assertThat(results.size()).isEqualTo(1);
     Row row = results.get(0);
     assertThat(row.getLong("bigintcol")).isEqualTo(5725368L);
+
+    // auth.provider was coerced to DSE
+    assertThat(task.getInstanceState().getConfig().getAuthenticatorConfig().getProvider())
+        .isEqualTo(Provider.DSE);
   }
 
-  @Test
-  void raw_bigint_value_bad_creds() {
-    Map<String, String> extras =
-        ImmutableMap.<String, String>builder()
-            .put(PROVIDER_OPT, "DSE")
-            .put(USERNAME_OPT, "cassandra")
-            .put(PASSWORD_OPT, "cassandra2")
-            .build();
-
+  @ParameterizedTest(name = "[{index}] extras={0}")
+  @MethodSource("incorrectCredentialsProvider")
+  void should_error_that_password_or_username_is_incorrect(Map<String, String> extras) {
     conn.start(makeConnectorProperties(extras));
 
     SinkRecord record = new SinkRecord("mytopic", 0, null, null, null, 5725368L, 1234L);
     assertThatThrownBy(() -> runTaskWithRecords(record))
         .isInstanceOf(AllNodesFailedException.class)
         .hasMessageContaining("and/or password are incorrect");
+  }
+
+  private static Stream<? extends Arguments> incorrectCredentialsProvider() {
+    ImmutableMap<String, String> incorrectCredentials =
+        ImmutableMap.<String, String>builder()
+            .put(USERNAME_OPT, "cassandra")
+            .put(PASSWORD_OPT, "cassandra2")
+            .build();
+
+    return Stream.of(
+        Arguments.of(
+            ImmutableMap.builder().putAll(incorrectCredentials).put(PROVIDER_OPT, "DSE").build()),
+        Arguments.of(
+            ImmutableMap.builder()
+                .putAll(incorrectCredentials)
+                .put(PROVIDER_OPT, "None")
+                .build()), // should infer auth.provider to DSE
+        Arguments.of(incorrectCredentials) // should infer auth.provider to DSE
+        );
+  }
+
+  private static Stream<? extends Arguments> correctCredentialsProvider() {
+    ImmutableMap<String, String> incorrectCredentials =
+        ImmutableMap.<String, String>builder()
+            .put(USERNAME_OPT, "cassandra")
+            .put(PASSWORD_OPT, "cassandra")
+            .build();
+
+    return Stream.of(
+        Arguments.of(
+            ImmutableMap.builder().putAll(incorrectCredentials).put(PROVIDER_OPT, "DSE").build()),
+        Arguments.of(
+            ImmutableMap.builder()
+                .putAll(incorrectCredentials)
+                .put(PROVIDER_OPT, "None")
+                .build()), // should infer auth.provider to DSE
+        Arguments.of(incorrectCredentials) // should infer auth.provider to DSE
+        );
   }
 }
