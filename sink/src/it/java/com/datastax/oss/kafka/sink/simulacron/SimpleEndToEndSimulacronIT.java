1b4aa2b188711632ed531afdbef83afde1a237f0
Enrico Olivelli <enrico.olivelli@datastax.com>
2023-07-19 08:48:10 +0200

    Fix test

--- sink/src/it/java/com/datastax/oss/kafka/sink/simulacron/SimpleEndToEndSimulacronIT.java
+++ sink/src/it/java/com/datastax/oss/kafka/sink/simulacron/SimpleEndToEndSimulacronIT.java
@@ -339,6 +339,7 @@ class SimpleEndToEndSimulacronIT {
             // since we upgraded to Driver 4.16.x, we need to explicitly set the protocol version
             // otherwise it will try only DSE_v1 and DSE_v2 because they are not considered "BETA"
             // https://github.com/datastax/java-driver/blob/4270f93277249abb513bc2abf2ff7a7c481b1d0d/core/src/main/java/com/datastax/oss/driver/internal/core/channel/ChannelFactory.java#L163
+            .put("datastax-java-driver.advanced.protocol.version", "V4")
             .put("topic.mytopic.ks1.mycounter.mapping", "a=key, b=value, c=value.f2")
             .build();
     assertThatThrownBy(() -> task.start(props))
@@ -371,6 +372,7 @@ class SimpleEndToEndSimulacronIT {
     // since we upgraded to Driver 4.16.x, we need to explicitly set the protocol version
     // otherwise it will try only DSE_v1 and DSE_v2 because they are not considered "BETA"
     // https://github.com/datastax/java-driver/blob/4270f93277249abb513bc2abf2ff7a7c481b1d0d/core/src/main/java/com/datastax/oss/driver/internal/core/channel/ChannelFactory.java#L163
+    connProps.put("datastax-java-driver.advanced.protocol.version", "V4");
     connProps.put("port", port);
     connProps.put("loadBalancing.localDc", "dc1");
     connProps.put(
